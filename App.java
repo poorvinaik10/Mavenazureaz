a052ccb60c633ddabcf7e4730949a926ffe509e7
poorvinaik10 <naikpoorvi32@gmail.com>
2025-05-28 04:32:03 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package program10.HelloWorls;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
